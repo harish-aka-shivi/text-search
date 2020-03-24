78021144fe27067dd9711d3058ef98362d789b5c
Harish Rana <r.shivi@yahoo.com>
2020-03-24 19:39:18 +0530

    feat: make working keyword search app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import originalSentences from './sentences.json';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const SearchBar = ({handleInput}) => (
+    <input placeholder="Type your keyword here" className="Search-bar" type="search" onInput={handleInput} />
+)
+
+const ListItem = ({ text }) => (
+  <li className="List-item">{text}</li>
+)
+
+const List = ({ sentences }) => (
+  <ul className="List">
+    {
+      sentences.map(sentence => (
+        <ListItem text={sentence.data} key={sentence.id}></ListItem>
+      ))
+    }
+  </ul>
+);
+
+const searchAlgorithm = (keyword) => originalSentences.filter(sentence => sentence.data.toLowerCase().indexOf(keyword.toLowerCase()) !== -1)
+
+
+const App = () => {
+  const [filteredList, setFilteredList] = useState(originalSentences);
+
+  const handleInput = (event) => {
+    const searchList = searchAlgorithm(event.target.value);
+    setFilteredList(searchList)
+  }
+
+  return(
+    <main>
+      <div className="App-root" role="presentation">
+        <SearchBar handleInput={handleInput}></SearchBar>
+        <List sentences={filteredList}></List>
+      </div>
+      
+    </main>
+  )
 }
 
 export default App;
